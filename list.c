71eea51debe11037dcdbbc016dbc1dc8c5b49534
Replit user <>
2023-08-24 05:54:59 +0000

    new try

--- list.c
+++ list.c
@@ -138,34 +138,31 @@ void * popCurrent(List * list) {
   if(list->current->next != NULL){
     list->current->next->prev=list->current->prev;
     
-    list->current->prev->next=list->current->next;
-
-    list->current=list->current->next;
-    return list->current;
-  }else{
-    list->current->prev=list->current->prev;
-    list->current->prev->next=list->current;
     
-    list->current=list->current->prev;
-    return list->current;
+
+  }else{
+    list->tail=list->current->prev;
+    
+ 
+    
   }
 
   
   if(list->current->prev != NULL){
     list->current->prev->next=list->current->next;
     
-    list->current->next->prev=list->current->prev;
+    
 
     list->current=list->current->prev;
-    return list->current;
+   
   }else{
-    list->current->next=list->current->next;
+    list->head=list->current->next;
     
-    list->current->next->prev=list->current;
+   
     
-    list->current=list->current->next;
-    return list->current;
+    
   }
+  list->current=list->current->next;
 }
 
 void cleanList(List * list) {
